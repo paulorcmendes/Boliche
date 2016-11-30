22855ffbdd2014704c8862a56aa64c8d3b5eaff0
paulorcmendes <mendesdpn@gmail.com>
2016-11-30 03:48:56 -0300

    First Commit

---
+++ Assets/scripts/ControleBola.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControleBola : MonoBehaviour
+{
+	private Rigidbody rb;
+	private bool play = false;
+	public float power;
+	private GameObject[] pinos;
+	private float[] xzes;
+	private int derrubNow;
+	public int derrubados;
+	public Text Placar;
+    // Use this for initialization
+    void Start()
+    {
+		pinos = GameObject.FindGameObjectsWithTag("pino");
+		xzes = new float[10];
+		for(int i = 0;i<xzes.Length;i++){
+			xzes[i]=pinos[i].transform.position.x;
+		}
+        derrubados = 0;
+        Placar.text ="";
+        rb = GetComponent<Rigidbody>();
+         }
+// Update is called once per frame
+void FixedUpdate()
+    {
+         if (Input.GetKey("a") && transform.position.x < 2.0f && !play)
+        {
+            transform.Translate(0.1f, 0.0f, 0.0f);
+             }
+         if (Input.GetKey("d") && transform.position.x > -2.0f && !play)
+        {
+            transform.Translate(-0.1f, 0.0f, 0.0f);
+             }
+         if (Input.GetKey("w") && !play)
+        {
+            rb.AddForce(new Vector3(0.0f, 0.0f, -Mathf.Abs(power)));
+            play = true;
+           }
+        if (play)
+        {
+			derrubNow = 0;
+			for (int i = 0; i < pinos.Length; i++) {
+				if (Mathf.Abs(pinos[i].transform.rotation.eulerAngles.z) > 45 || Mathf.Abs(pinos[i].transform.rotation.eulerAngles.x) > 45)
+					derrubNow++;
+			}
+			if (derrubNow > derrubados)
+				derrubados = derrubNow;
+            Placar.text = "Derrubou: " + derrubados.ToString() + " pinos.";
+        }
+    }
+}
---
+++ Assets/scripts/ControleCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+ using System.Collections;
+ public class ControleCamera : MonoBehaviour
+{
+ public GameObject bola;
+ private Vector3 offset;
+ // Use this for initialization
+ void Start()
+ {
+ offset = transform.position - bola.transform.position;
+ }
+ // Update is called once per frame
+void LateUpdate() {
+ transform.position = bola.transform.position + offset;
+}
+}
